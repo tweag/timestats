d3cb36b54872bd3a59ca20fb6542d9ea2ce374fa
Facundo Domínguez <facundo.dominguez@tweag.io>
2025-07-23 00:43:08 +0000

    Force clock sampling to happen in order
    The lazy State monad allowed for time measures to occur in a different
    order otherwise.

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,10 +1,13 @@
+{-# OPTIONS_GHC -Wno-x-partial #-}
 module Main where
 
 import Control.Exception (evaluate)
 import Control.Monad (unless)
+import Control.Monad.State
 import qualified Data.Text.IO as Text
 import qualified Debug.TimeStats as TimeStats
 import qualified Debug.TimeStats.Internal as Internal
+import qualified Debug.TimeStats.Unsafe as TimeStats
 import System.Environment (setEnv)
 import System.Exit (exitFailure)
 
@@ -14,6 +17,7 @@ fib n = if n < 2 then 1 else fib (n - 1) + fib (n - 2)
 main :: IO ()
 main = do
     testMeasureM
+    testUnsafeMeasureM
     testFormatIntWithSeparator
 
 testMeasureM :: IO ()
@@ -28,13 +32,9 @@ testMeasureM = do
           , ("fib2", TimeStats.TimeStats 0 2)
           ]
     unless (eqStats xs expected) $ do
-      putStrLn "unexpected timestats:"
+      putStrLn "measureM: unexpected timestats:"
       Text.putStrLn (TimeStats.asText xs)
       exitFailure
-  where
-    eqStats xs ys = length xs == length ys && and (zipWith eqStat xs ys)
-    eqStat (lbl0, ts0) (lbl1, ts1) =
-      lbl0 == lbl1 && TimeStats.countStat ts0 == TimeStats.countStat ts1
 
 testFormatIntWithSeparator :: IO ()
 testFormatIntWithSeparator = do
@@ -65,3 +65,26 @@ testFormatIntWithSeparator = do
         putStrLn $ "expected: " ++ show (expected ++ "a")
         putStrLn $ "  actual: " ++ show actual
         exitFailure
+
+testUnsafeMeasureM :: IO ()
+testUnsafeMeasureM = do
+    setEnv "DEBUG_TIMESTATS_ENABLE" "1"
+    TimeStats.reset
+    evaluate $ (`execState` 0) $
+      TimeStats.unsafeMeasureM "fib" $ put (fib 40)
+    xs <- TimeStats.collect
+    let expected =
+          [ ("fib", TimeStats.TimeStats 0 1)
+          , ("fib2", TimeStats.TimeStats 0 0)
+          ]
+    -- A large time value indicates an error
+    unless (eqStats xs expected
+             || TimeStats.timeStat (snd $ head xs) > 1000000000) $ do
+      putStrLn "unsafeMeasureM: unexpected timestats:"
+      Text.putStrLn (TimeStats.asText xs)
+      exitFailure
+
+eqStats :: Eq a => [(a, TimeStats.TimeStats)] -> [(a, TimeStats.TimeStats)] -> Bool
+eqStats xs ys = length xs == length ys && and (zipWith eqStat xs ys)
+eqStat (lbl0, ts0) (lbl1, ts1) =
+    lbl0 == lbl1 && TimeStats.countStat ts0 == TimeStats.countStat ts1
